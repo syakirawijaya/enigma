4313a5425303069cc323564ba657453296f70c45
syakirawijaya <90813516+syakirawijaya@users.noreply.github.com>
2024-11-26 14:04:58 +0000

    commit 2

--- enigma.py
+++ enigma.py
@@ -2,4 +2,12 @@ import random
 from datetime import date
 
 randomnum = random.randint(00000,99999)
+keylist = {}
 
+keylist["A"] = randomnum[0,2]
+keylist["B"] = randomnum[1,3]
+keylist["C"] = randomnum[2,4]
+keylist["D"] = randomnum[3,]
+
+date = date.today
+offset = date.strftime("%d%m%Y")
